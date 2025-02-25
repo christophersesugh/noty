e93b48f566a3f925647571bd2f871e2d92c42de2
christophersesugh <christohybrid185@gmail.com>
2025-02-25 10:48:44 +0100

    remove .env file from git

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -14,8 +14,6 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 import Spinner from '../components/Spinner';
 
 const API_KEY = import.meta.env.VITE_APP_GEMINI_API_KEY;
-// ??
-// process.env.VITE_APP_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 const CreateNote = ({ setNotes }) => {
